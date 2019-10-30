57160674de5ddaca55295b1b13ece18dd242ca8d
Oleksii Solomakha <alexssolomakha@gmail.com>
2019-10-30 16:10:28 +0200

    update url

--- scripts/data.js
+++ scripts/data.js
@@ -3,7 +3,7 @@ var data = [
 		id: 1,
 		title: "Color Game",
 		description: "The great RGB guessing color game. Built on pure js.",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/Color-Game/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/Color-Game/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/Color-Game",
 		image: "images/works-screen/color-game.jpg",
 		tags: ["Html/Css", "JavaScript"]
@@ -12,7 +12,7 @@ var data = [
 		id: 2,
 		title: "Todo List",
 		description: "Another simple Todo list",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/TODO-List/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/TODO-List/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/TODO-List",
 		image: "images/works-screen/todo.jpg",
 		tags: ["Html/Css", "Bootstrap", "JavaScript"]
@@ -21,7 +21,7 @@ var data = [
 		id: 3,
 		title: "The Weather App",
 		description: "Classic Weather App project used AcuWeather Apis. With async request to the server, we get information about location and current weather condition.",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/Weather-app/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/Weather-app/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/Weather-App",
 		image: "images/works-screen/weather.jpg",
 		tags: ["Html/Css", "Bootstrap", "JavaScript", "AJAX"]
@@ -30,7 +30,7 @@ var data = [
 		id: 4,
 		title: "Interactive Quiz",
 		description: "Simple and slightly animated quiz. Check how many percent you use your brain.",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/Quiz/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/Quiz/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/Quiz",
 		image: "images/works-screen/quiz.jpg",
 		tags: ["Html/Css", "Bootstrap", "JavaScript"]
@@ -39,7 +39,7 @@ var data = [
 		id: 5,
 		title: "Magic Ball",
 		description: "Have a question? Shake the 8 ball and get an answer!",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/Magic-Ball/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/Magic-Ball/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/Magic-Ball",
 		image: "images/works-screen/ball.jpg",
 		tags: ["Html/Css", "JavaScript"]
@@ -48,7 +48,7 @@ var data = [
 		id: 6,
 		title: "Real time chatroom",
 		description: "Real time chatroom, can be updated on different devices at the same time, with a different rooms in it.",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/Chat-Room/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/Chat-Room/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/Chat-Room",
 		image: "images/works-screen/chat-room.jpg",
 		tags: ["Html/Css", "Bootstrap", "JavaScript", "Firebase"]
@@ -57,7 +57,7 @@ var data = [
 		id: 7,
 		title: "Digital watch",
 		description: "Sreen digital clock. Just another way to get a Date().",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/Clock/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/Clock/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/Clock",
 		image: "images/works-screen/clock.jpg",
 		tags: ["Html/Css", "JavaScript"]
@@ -66,7 +66,7 @@ var data = [
 		id: 8,
 		title: "Snake Game",
 		description: "Another version of this legendary game.",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/Snake/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/Snake/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/Snake-Game",
 		image: "images/works-screen/snake.jpg",
 		tags: ["Html/Css", "JavaScript", "jQuery"]
@@ -75,7 +75,7 @@ var data = [
 		id: 9,
 		title: "UI Library",
 		description: "Simple UI with interesting features. Used Node JS to install all modules and packages for work: babel, webpack etc.",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/Ul-Library/dist/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/Ul-Library/dist/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/UI-Library",
 		image: "images/works-screen/ui-library.jpg",
 		tags: ["Html/Css", "JavaScript", "NodeJS", "Webpack"]
@@ -84,7 +84,7 @@ var data = [
 		id: 10,
 		title: "Todo List App",
 		description: "One more Todo List made with jQuery.",
-		linkToDemo: "https://github.com/oleksiisolomakha.github.io/works/jQuery-Todo-App/index.html",
+		linkToDemo: "https://oleksiisolomakha.github.io/works/jQuery-Todo-App/index.html",
 		linkToCode: "https://github.com/OleksiiSolomakha/jQuery-Todo-App",
 		image: "images/works-screen/jQuery-todo.jpg",
 		tags: ["Html/Css", "Bootstrap", "JavaScript"]
@@ -94,7 +94,7 @@ var data = [
 		title: "Portfolio",
 		description: "this.Portfolio. Almost all DOM generates by JS. Build with js classes.",
 		linkToDemo: "http://oleksiisolomakha.github.io/works/Portfolio/index.html",
-		linkToCode: "https://github.com/oleksiisolomakha.github.io",
+		linkToCode: "https://oleksiisolomakha.github.io",
 		image: "images/works-screen/portfolio.jpg",
 		tags: ["Html/Css", "JavaScript", "jQuery"]
 	}
